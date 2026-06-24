1dcc90c98abd6e66b0e9780adca1816b15468804
Jose Luis Munoz Zuta <joseluismunozzuta@gmail.com>
2026-06-24 02:59:10 -0500

    fix netlify build

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  webpack: (config) => {
-    config.watchOptions = { ...config.watchOptions, ignored: /functions/ };
-    return config;
-  },
 };
 
 export default nextConfig;
